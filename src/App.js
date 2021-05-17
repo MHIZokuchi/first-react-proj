fa4674b3c86242c60ba793576d4416d6c664d85b
Okwuchi Uzoigwe <P7927@fidelitybank.ng>
2021-05-17 16:16:29 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Item from './Components/Item';
+import Section from './Components/Section';
 
 function App() {
+
+  const names = String("Ade;Fatima;Daniel;Gloria:Samuel").split(';');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+<Header/>
+<main>
+<Section/>
+  <div className="album py-5 bg-light">
+    <div className="container">
+
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+      {
+        names.map(v => <Item title={v}/>)
+      }
+     
+  
+      </div>
     </div>
+  </div>
+
+</main>
+
+<Footer/>
+    </>
   );
 }
 
